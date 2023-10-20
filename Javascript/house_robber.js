43103c41329817eec90a9574ccea6cf23c9ca050
PallaviKhedle <pallavi8khedle@gmail.com>
2023-10-19 22:16:36 -0700

    robber leetcode problem

---
+++ Javascript/house_robber.js
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/house-robber/
+
+var rob = function(nums) {
+    let evenSum = 0; oddSum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (i % 2 === 0) evenSum = Math.max(evenSum + nums[i], oddSum);
+        else oddSum = Math.max(oddSum + nums[i], evenSum);  
+    }
+    return Math.max(evenSum, oddSum);
+};
